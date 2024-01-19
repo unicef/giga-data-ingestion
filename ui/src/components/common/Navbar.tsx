8669fab9dea602c92cccb89fd275cbcfcc8065ef
GERLITO CHAGAS <ger@thinkingmachin.es>
2024-01-19 17:17:38 +0800

    fix: update admin panel navbar color

--- ui/src/components/common/Navbar.tsx
+++ ui/src/components/common/Navbar.tsx
@@ -8,40 +8,57 @@ export default function Navbar() {
   const { featureFlags } = useStore();
   const location = useLocation();
 
-  const linkName =
-    location.pathname === "/user-management"
-      ? "Ingestion Portal"
-      : "Admin Panel";
-
-  const redirectLink =
-    location.pathname === "/user-management" ? "/" : "/user-management";
+  const isUserManagementPage = location.pathname === "/user-management";
 
   return (
     <header className="flex-none">
-      <nav className="flex h-[80px] items-center justify-between bg-primary p-4 text-white">
-        <Link
-          to="/"
-          className="flex items-center gap-2"
-          unstable_viewTransition
-        >
-          <img src="/GIGA_logo.png" alt="Giga" />
-          <h1 className="text-2xl text-white">
-            <b>giga ingest</b>
-          </h1>
-        </Link>
+      {isUserManagementPage ? (
+        <nav className="flex h-[80px] items-center justify-between border-b-4 border-primary bg-white p-4 text-primary">
+          <Link
+            to="/"
+            className="flex items-center gap-2"
+            unstable_viewTransition
+          >
+            <img src="/GIGA_logo_blue.png" alt="Giga" />
+            <h1 className="text-2xl text-primary">
+              <b>giga ingest</b>
+            </h1>
+          </Link>
 
-        <AuthenticatedTemplate>
-          {featureFlags.userManagementPage && (
-            <Link
-              to={redirectLink}
-              unstable_viewTransition
-              className="text-white"
-            >
-              {linkName}
-            </Link>
-          )}
-        </AuthenticatedTemplate>
-      </nav>
+          <AuthenticatedTemplate>
+            {featureFlags.userManagementPage && (
+              <Link to="/" unstable_viewTransition className="text-primary">
+                Ingestion Portal
+              </Link>
+            )}
+          </AuthenticatedTemplate>
+        </nav>
+      ) : (
+        <nav className="flex h-[80px] items-center justify-between bg-primary p-4 text-white">
+          <Link
+            to="/"
+            className="flex items-center gap-2"
+            unstable_viewTransition
+          >
+            <img src="/GIGA_logo.png" alt="Giga" />
+            <h1 className="text-2xl text-white">
+              <b>giga ingest</b>
+            </h1>
+          </Link>
+
+          <AuthenticatedTemplate>
+            {featureFlags.userManagementPage && (
+              <Link
+                to="/user-management"
+                unstable_viewTransition
+                className="text-white"
+              >
+                Admin Panel
+              </Link>
+            )}
+          </AuthenticatedTemplate>
+        </nav>
+      )}
     </header>
   );
 }
