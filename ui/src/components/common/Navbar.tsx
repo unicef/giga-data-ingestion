a1d31a0ab3908db7361f8b586beba7bb6aa245c7
GERLITO CHAGAS <ger@thinkingmachin.es>
2024-01-22 18:42:39 +0800

    refactor: use clsx for classes

--- ui/src/components/common/Navbar.tsx
+++ ui/src/components/common/Navbar.tsx
@@ -1,6 +1,7 @@
 import { Link, useLocation } from "react-router-dom";
 
 import { AuthenticatedTemplate } from "@azure/msal-react";
+import clsx from "clsx";
 
 import { useStore } from "@/store.ts";
 
@@ -10,55 +11,48 @@ export default function Navbar() {
 
   const isUserManagementPage = location.pathname === "/user-management";
 
+  const navClass = clsx("flex h-[80px] items-center justify-between", {
+    "border-b-4 border-primary bg-white p-4 ": isUserManagementPage,
+    " bg-primary p-4 text-white": !isUserManagementPage,
+  });
+
+  const linkClass = clsx("flex items-center gap-2", {
+    "text-primary": isUserManagementPage,
+    "text-white": !isUserManagementPage,
+  });
+
+  const headerClass = clsx("text-2xl", {
+    "text-primary": isUserManagementPage,
+    "text-white": !isUserManagementPage,
+  });
+
   return (
     <header className="flex-none">
-      {isUserManagementPage ? (
-        <nav className="flex h-[80px] items-center justify-between border-b-4 border-primary bg-white p-4 text-primary">
-          <Link
-            to="/"
-            className="flex items-center gap-2"
-            unstable_viewTransition
-          >
-            <img src="/GIGA_logo_blue.png" alt="Giga" />
-            <h1 className="text-2xl text-primary">
-              <b>giga ingest</b>
-            </h1>
-          </Link>
+      <nav className={navClass}>
+        <Link to="/" className={linkClass} unstable_viewTransition>
+          <img
+            src={
+              isUserManagementPage ? "/GIGA_logo_blue.png" : "/GIGA_logo.png"
+            }
+            alt="Giga"
+          />
+          <h1 className={headerClass}>
+            <b>giga ingest</b>
+          </h1>
+        </Link>
 
-          <AuthenticatedTemplate>
-            {featureFlags.userManagementPage && (
-              <Link to="/" unstable_viewTransition className="text-primary">
-                Ingestion Portal
-              </Link>
-            )}
-          </AuthenticatedTemplate>
-        </nav>
-      ) : (
-        <nav className="flex h-[80px] items-center justify-between bg-primary p-4 text-white">
-          <Link
-            to="/"
-            className="flex items-center gap-2"
-            unstable_viewTransition
-          >
-            <img src="/GIGA_logo.png" alt="Giga" />
-            <h1 className="text-2xl text-white">
-              <b>giga ingest</b>
-            </h1>
-          </Link>
-
-          <AuthenticatedTemplate>
-            {featureFlags.userManagementPage && (
-              <Link
-                to="/user-management"
-                unstable_viewTransition
-                className="text-white"
-              >
-                Admin Panel
-              </Link>
-            )}
-          </AuthenticatedTemplate>
-        </nav>
-      )}
+        <AuthenticatedTemplate>
+          {featureFlags.userManagementPage && (
+            <Link
+              to={isUserManagementPage ? "/" : "/user-management"}
+              unstable_viewTransition
+              className={linkClass}
+            >
+              {isUserManagementPage ? "Ingestion Portal" : "Admin Panel"}
+            </Link>
+          )}
+        </AuthenticatedTemplate>
+      </nav>
     </header>
   );
 }
