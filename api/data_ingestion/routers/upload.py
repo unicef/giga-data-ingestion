21418e8eb387f57a933b9c55dd60fa2eb6135bdf
GERLITO CHAGAS <ger@thinkingmachin.es>
2024-01-24 17:22:20 +0800

    fix: move metadata from query to body

--- api/data_ingestion/routers/upload.py
+++ api/data_ingestion/routers/upload.py
@@ -1,11 +1,13 @@
 from datetime import datetime
+from typing import Annotated
 from uuid import uuid4
 
 import country_converter as coco
 from azure.core.exceptions import HttpResponseError
 from fastapi import (
     APIRouter,
-    Depends,
+    File,
+    Form,
     HTTPException,
     Response,
     Security,
@@ -16,7 +18,6 @@ from fastapi import (
 from data_ingestion.constants import constants
 from data_ingestion.internal.auth import azure_scheme
 from data_ingestion.internal.storage import storage_client
-from data_ingestion.schemas.upload import UploadFileMetadata
 
 router = APIRouter(
     prefix="/api/upload",
@@ -30,7 +31,17 @@ async def upload_file(
     response: Response,
     file: UploadFile,
     dataset: str,
-    metadata: UploadFileMetadata = Depends(),
+    sensitivity_level: Annotated[str, Form()],
+    pii_classification: Annotated[str, Form()],
+    geolocation_data_source: Annotated[str, Form()],
+    data_collection_modality: Annotated[str, Form()],
+    domain: Annotated[str, Form()],
+    date_modified: Annotated[str, Form()],
+    source: Annotated[str, Form()],
+    data_owner: Annotated[str, Form()],
+    country: Annotated[str, Form()],
+    school_id_type: Annotated[str, Form()],
+    description_file_update: Annotated[str, Form()],
 ):
     if file.size > constants.UPLOAD_FILE_SIZE_LIMIT:
         raise HTTPException(
@@ -39,17 +50,28 @@ async def upload_file(
         )
     uid = str(uuid4())
 
-    country_code = coco.convert(metadata.country, to="ISO3")
+    country_code = coco.convert(country, to="ISO3")
     timestamp = datetime.utcnow().strftime("%Y%m%d-%H%M%S")
 
-    filename = (
-        f"raw/uploads/{uid}_{country_code}_"
-        f"{dataset}_{metadata.source}-{timestamp}"
-    )
+    filename = f"raw/uploads/{uid}_{country_code}_" f"{dataset}_{source}-{timestamp}"
     client = storage_client.get_blob_client(filename)
 
     try:
-        client.upload_blob(await file.read(), metadata=metadata.dict())
+        metadata = {
+            "sensitivity_level": sensitivity_level,
+            "pii_classification": pii_classification,
+            "geolocation_data_source": geolocation_data_source,
+            "data_collection_modality": data_collection_modality,
+            "domain": domain,
+            "date_modified": date_modified,
+            "source": source,
+            "data_owner": data_owner,
+            "country": country,
+            "school_id_type": school_id_type,
+            "description_file_update": description_file_update,
+        }
+
+        client.upload_blob(await file.read(), metadata=metadata)
         response.status_code = status.HTTP_201_CREATED
     except HttpResponseError as err:
         raise HTTPException(
