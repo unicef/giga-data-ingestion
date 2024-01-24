568462343e45dadfaf160800b4df798c53e9f10f
Kenneth V. Domingo <kenneth@thinkingmachin.es>
2024-01-24 21:24:54 +0800

    style: [PARTIAL] convert AddUserModal

--- ui/src/utils/string.ts
+++ ui/src/utils/string.ts
@@ -43,3 +43,38 @@ export const getUniqueDatasets = (
 
   return result;
 };
+
+export const getUniqueDatasetsNew = (
+  email: string,
+  addedDatasets: Array<{ name: string; id?: string }>,
+) => {
+  const uniqueCountries = new Set<string>();
+  const datasetTypes = new Set();
+
+  addedDatasets.forEach(item => {
+    const country = item.name.split("-")[0];
+    uniqueCountries.add(country);
+
+    if (item.name.includes("School Geolocation")) {
+      datasetTypes.add("School Geolocation");
+    }
+    if (item.name.includes("School Coverage")) {
+      datasetTypes.add("School Coverage");
+    }
+    if (item.name.includes("School QoS")) {
+      datasetTypes.add("School QoS");
+    }
+  });
+
+  const result: {
+    countries: string[];
+    email: string;
+    uniqueDatasets: number;
+  } = {
+    countries: Array.from(uniqueCountries),
+    email: email,
+    uniqueDatasets: datasetTypes.size,
+  };
+
+  return result;
+};
