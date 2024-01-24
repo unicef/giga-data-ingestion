1aa26a43db5e5bbf9dd93b147c0447f4d07596e7
tm-ger-chagas <122899250+tm-ger-chagas@users.noreply.github.com>
2024-01-24 17:29:33 +0800

    [US-16-105-01] Upload file to ADLS (#11)
    * refactor: add try catch to upload
    
    * feat: allow overwriting of file
    
    * feat: change upload location
    
    * chore: set linter line length
    
    * chore: make ruff not auto lint unused imports
    
    * feat: add country converter
    
    * feat: update metadata input and rename file on upload
    
    * fix: revert lint changes
    
    * chore: addd coco poetry lock
    
    * fix: move metadata from query to body

--- api/data_ingestion/routers/upload.py
+++ api/data_ingestion/routers/upload.py
@@ -1,15 +1,19 @@
+from datetime import datetime
+from typing import Annotated
 from uuid import uuid4
 
+import country_converter as coco
+from azure.core.exceptions import HttpResponseError
 from fastapi import (
     APIRouter,
-    Depends,
+    File,
+    Form,
     HTTPException,
     Response,
     Security,
     UploadFile,
     status,
 )
-from fastapi_azure_auth.user import User
 
 from data_ingestion.constants import constants
 from data_ingestion.internal.auth import azure_scheme
@@ -24,7 +28,20 @@ router = APIRouter(
 
 @router.post("")
 async def upload_file(
-    response: Response, file: UploadFile, user: User = Depends(azure_scheme)
+    response: Response,
+    file: UploadFile,
+    dataset: str,
+    sensitivity_level: Annotated[str, Form()],
+    pii_classification: Annotated[str, Form()],
+    geolocation_data_source: Annotated[str, Form()],
+    data_collection_modality: Annotated[str, Form()],
+    domain: Annotated[str, Form()],
+    date_modified: Annotated[str, Form()],
+    source: Annotated[str, Form()],
+    data_owner: Annotated[str, Form()],
+    country: Annotated[str, Form()],
+    school_id_type: Annotated[str, Form()],
+    description_file_update: Annotated[str, Form()],
 ):
     if file.size > constants.UPLOAD_FILE_SIZE_LIMIT:
         raise HTTPException(
@@ -32,12 +49,31 @@ async def upload_file(
             detail="File size exceeds 10 MB limit",
         )
     uid = str(uuid4())
-    metadata = {
-        "id": uid,
-        "uploader": user.email or user.preferred_username,
-        "original_filename": file.filename,
-    }
-    filename = f"{user.sub}/{uid[:8]}-{file.filename}"
+
+    country_code = coco.convert(country, to="ISO3")
+    timestamp = datetime.utcnow().strftime("%Y%m%d-%H%M%S")
+
+    filename = f"raw/uploads/{uid}_{country_code}_" f"{dataset}_{source}-{timestamp}"
     client = storage_client.get_blob_client(filename)
-    client.upload_blob(await file.read(), metadata=metadata)
-    response.status_code = status.HTTP_201_CREATED
+
+    try:
+        metadata = {
+            "sensitivity_level": sensitivity_level,
+            "pii_classification": pii_classification,
+            "geolocation_data_source": geolocation_data_source,
+            "data_collection_modality": data_collection_modality,
+            "domain": domain,
+            "date_modified": date_modified,
+            "source": source,
+            "data_owner": data_owner,
+            "country": country,
+            "school_id_type": school_id_type,
+            "description_file_update": description_file_update,
+        }
+
+        client.upload_blob(await file.read(), metadata=metadata)
+        response.status_code = status.HTTP_201_CREATED
+    except HttpResponseError as err:
+        raise HTTPException(
+            detail=err.message, status_code=err.response.status_code
+        ) from err
