3f016037163776c156323e49d0ef2bc667b1021b
Kenneth V. Domingo <kenneth@thinkingmachin.es>
2024-01-24 14:16:57 +0800

    style: add header panel

--- ui/src/components/common/Navbar.tsx
+++ ui/src/components/common/Navbar.tsx
@@ -1,21 +1,29 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import { AuthenticatedTemplate } from "@azure/msal-react";
+import { Switcher as SwitcherIcon } from "@carbon/icons-react";
 import {
   Header,
+  HeaderGlobalAction,
+  HeaderGlobalBar,
   HeaderMenuItem,
   HeaderName,
   HeaderNavigation,
+  HeaderPanel,
+  Switcher,
+  SwitcherItem,
 } from "@carbon/react";
 
 import { useStore } from "@/store.ts";
 
 export default function Navbar() {
   const { featureFlags } = useStore();
+  const [isSwitcherExpanded, setIsSwitcherExpanded] = useState(false);
 
   return (
     <Header className="relative">
-      <HeaderName href="/" prefix="">
+      <HeaderName as={Link} to="/" unstable_viewTransition prefix="">
         <img src="/GIGA_logo_blue.png" className="h-5/6" alt="Giga" />
         <b>giga</b>ingest
       </HeaderName>
@@ -32,6 +40,39 @@ export default function Navbar() {
           </HeaderNavigation>
         )}
       </AuthenticatedTemplate>
+      <HeaderGlobalBar>
+        <HeaderGlobalAction
+          onClick={() => setIsSwitcherExpanded(expanded => !expanded)}
+        >
+          <SwitcherIcon />
+        </HeaderGlobalAction>
+      </HeaderGlobalBar>
+      <HeaderPanel
+        expanded={isSwitcherExpanded}
+        onHeaderPanelFocus={() => setIsSwitcherExpanded(expanded => !expanded)}
+      >
+        <Switcher aria-label="switcher container" expanded={isSwitcherExpanded}>
+          <SwitcherItem aria-label="upload file">
+            <Link to="/upload" unstable_viewTransition>
+              Upload File
+            </Link>
+          </SwitcherItem>
+          <SwitcherItem aria-label="upload file">
+            <Link to="/datasources" unstable_viewTransition>
+              Ingest API
+            </Link>
+          </SwitcherItem>
+          {featureFlags.userManagementPage && (
+            <AuthenticatedTemplate>
+              <SwitcherItem aria-label="upload file">
+                <Link to="/user-management" unstable_viewTransition>
+                  Admin Panel
+                </Link>
+              </SwitcherItem>
+            </AuthenticatedTemplate>
+          )}
+        </Switcher>
+      </HeaderPanel>
     </Header>
   );
 }
