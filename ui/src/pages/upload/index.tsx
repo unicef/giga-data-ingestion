fe8f122c59ad00053d4d0347a43ab0437a88e960
Kenneth V. Domingo <kenneth@thinkingmachin.es>
2024-01-24 14:51:59 +0800

    style: use stack for upload accordion

--- ui/src/pages/upload/index.tsx
+++ ui/src/pages/upload/index.tsx
@@ -1,31 +1,35 @@
 import { Link } from "react-router-dom";
 
-import { Accordion, AccordionItem, Heading, Section } from "@carbon/react";
+import {
+  Accordion,
+  AccordionItem,
+  Heading,
+  Section,
+  Stack,
+} from "@carbon/react";
 
 export default function UploadDataSelect() {
   return (
     <Section>
-      <Heading>What will you be uploading today?</Heading>
       <Section>
-        <Accordion>
-          <AccordionItem title="A completely new dataset">
-            Create a new dataset
-          </AccordionItem>
-          <AccordionItem title="An addition to an existing dataset">
-            <ul>
-              <li>
+        <Stack gap={6}>
+          <Heading>What will you be uploading today?</Heading>
+          <Accordion>
+            <AccordionItem title="A completely new dataset">
+              Create a new dataset
+            </AccordionItem>
+            <AccordionItem title="An addition to an existing dataset">
+              <Stack gap={2}>
                 <Link to="school-data/geolocation" unstable_viewTransition>
                   School Geolocation
                 </Link>
-              </li>
-              <li>
                 <Link to="school-data/geolocation" unstable_viewTransition>
                   School Coverage
                 </Link>
-              </li>
-            </ul>
-          </AccordionItem>
-        </Accordion>
+              </Stack>
+            </AccordionItem>
+          </Accordion>
+        </Stack>
       </Section>
     </Section>
   );
