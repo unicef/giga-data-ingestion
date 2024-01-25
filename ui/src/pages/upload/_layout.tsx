011e25509b2106e145e80ec7dcdac0d4b4412662
GERLITO CHAGAS <ger@thinkingmachin.es>
2024-01-25 21:34:51 +0800

    style: increase padding

--- ui/src/pages/upload/_layout.tsx
+++ ui/src/pages/upload/_layout.tsx
@@ -8,7 +8,7 @@ export default function Upload() {
   return (
     <AuthenticatedTemplate>
       <div className="m-0 flex w-full flex-col gap-4 py-6">
-        <div className="px-28">
+        <div className="px-36">
           <UploadBreadcrumbs />
           <Outlet />
         </div>
