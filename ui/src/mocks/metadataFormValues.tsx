c9576b2e693864c88e3d88d78e308d4c904906a1
GERLITO CHAGAS <ger@thinkingmachin.es>
2024-01-25 21:34:38 +0800

    feat: add mock metadataForm optiopns

---
+++ ui/src/mocks/metadataFormValues.tsx
@@ -0,0 +1,28 @@
+export const sensitivityOptions = [
+  { value: "sensitive", label: "Sensitive" },
+  { value: "notSensitive", label: "Not Sensitive" },
+];
+
+export const piiOptions = [
+  { value: "containsPii", label: "Contains PII" },
+  { value: "doesNotContainPii", label: "Does not contain PII" },
+];
+
+export const geolocationDataSourceOptions = [{ value: "olm", label: "OSM" }];
+
+export const dataCollectionModalityOptions = [
+  { value: "online", label: "Online" },
+  { value: "offline", label: "Offline" },
+];
+
+export const domainOptions = [
+  { value: "infrastructure", label: "Infrastructure" },
+];
+
+export const sourceOptions = [{ value: "itu", label: "ITU" }];
+
+export const dataOwnerOptions = [
+  { value: "gerlito-uuid", label: "Gerlito Chagas" },
+];
+
+export const schoolIdTypeOptions = [{ value: "emis", label: "EMIS" }];
