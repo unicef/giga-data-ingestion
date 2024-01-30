3dba46e13c294d57a6a68c5b8f1eabe54cfec18f
GERLITO CHAGAS <ger@thinkingmachin.es>
2024-01-30 09:59:13 +0800

    feat: add mocks

--- api/data_ingestion/routers/upload.py
+++ api/data_ingestion/routers/upload.py
@@ -1,3 +1,4 @@
+import asyncio
 from datetime import datetime
 from typing import Annotated
 from uuid import uuid4
@@ -6,7 +7,6 @@ import country_converter as coco
 from azure.core.exceptions import HttpResponseError
 from fastapi import (
     APIRouter,
-    File,
     Form,
     HTTPException,
     Response,
@@ -77,3 +77,108 @@ async def upload_file(
         raise HTTPException(
             detail=err.message, status_code=err.response.status_code
         ) from err
+
+
+@router.get(
+    "",
+)
+async def list_column_checks():
+    await asyncio.sleep(2)
+
+    column_checks_headers = [
+        {"key": "columnName", "header": "Column name"},
+        {"key": "expectedDataType", "header": "Expected Data Type"},
+        {"key": "inDataset", "header": "Is the column in the dataset?"},
+        {"key": "isCorrectLocation", "header": "Is the column in the right data type?"},
+        {"key": "nullValues", "header": "How many null values per column?"},
+        {"key": "uniqueValues", "header": "How many unique values per column?"},
+    ]
+
+    column_checks_rows = [
+        {
+            "id": key,
+            "columnName": key,
+            "columnDescription": f"some description for cell -{key}",
+            "expectedDataType": "String",
+            "inDataset": "Not Run",
+            "isCorrectLocation": "Not Run",
+            "nullValues": "Not Run",
+            "uniqueValues": "Not Run",
+        }
+        for key in [
+            "school_id",
+            "school_name",
+            "education_level",
+            "internet_availability_type",
+            "mobile_internet_generation",
+            "internet_speed",
+            "computer_availability",
+            "school_year",
+            "latitude",
+            "longitude",
+        ]
+    ]
+
+    duplicate_rows_check_headers = [
+        {"key": "check", "header": "Check"},
+        {"key": "count", "header": "Count"},
+    ]
+
+    duplicate_rows_check_rows = [
+        {
+            "id": key,
+            "check": key,
+            "count": "Not Run",
+            "inDataset": "Not Run",
+            "isCorrectLocation": "Not Run",
+            "nullValues": "Not Run",
+            "uniqueValues": "Not Run",
+        }
+        for key in [
+            "Suspected duplicate rows with everything same except school code (dupx)",
+            "Suspected duplicate rows with same school id, education level, school name, lat-lon (dup0)",
+            "Suspected duplicate rows with same school name, education level, lat-lon (dup1)",
+            "Suspected duplicate rows with same education level,lat-lon (dup2)",
+            "Suspected duplicate rows with same school name and education level within 110m radius (dup3)",
+            "Suspected duplicate rows with similar school name and same education level within 110m radius (dup4)",
+        ]
+    ]
+
+    geospatial_data_points_checks_headers = [
+        {"key": "check", "header": "Check"},
+        {"key": "count", "header": "Count"},
+    ]
+
+    geospatial_data_points_checks_rows = [
+        {
+            "id": key,
+            "check": key,
+            "count": "Not Run",
+            "inDataset": "Not Run",
+            "isCorrectLocation": "Not Run",
+            "nullValues": "Not Run",
+            "uniqueValues": "Not Run",
+        }
+        for key in [
+            "Schools outside country boundary",
+            "Schools that have more than 5 schools within 70 square metre area (school_density_outlier_flag)",
+            "Rows with latitude values with less than satisfactory precision (5 digits): 10",
+        ]
+    ]
+
+    upload_checks = {
+        "column_checks": {
+            "headers": column_checks_headers,
+            "rows": column_checks_rows,
+        },
+        "duplicate_rows": {
+            "headers": duplicate_rows_check_headers,
+            "rows": duplicate_rows_check_rows,
+        },
+        "geospatial_data_points": {
+            "headers": geospatial_data_points_checks_headers,
+            "rows": geospatial_data_points_checks_rows,
+        },
+    }
+
+    return upload_checks
