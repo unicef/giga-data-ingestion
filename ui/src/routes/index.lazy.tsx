2cb9aeb53654b655008ffa6aad2af7947e57f4ce
Kenneth V. Domingo <kenneth@thinkingmachin.es>
2024-02-07 21:39:28 +0800

    feat(ui): correctly parse groups claim

--- ui/src/routes/index.lazy.tsx
+++ ui/src/routes/index.lazy.tsx
@@ -6,13 +6,11 @@ import { createLazyFileRoute } from "@tanstack/react-router";
 
 import Landing from "@/components/landing/Landing.tsx";
 import Login from "@/components/landing/Login.tsx";
-import FetchUserGroups from "@/components/utils/FetchUserGroups.tsx";
 
 export const Route = createLazyFileRoute("/")({
   component: () => (
     <>
       <AuthenticatedTemplate>
-        <FetchUserGroups />
         <Landing />
       </AuthenticatedTemplate>
 
