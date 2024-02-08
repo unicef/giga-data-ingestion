d6814eb65b18704ad8851caf956d08629a00950c
Kenneth V. Domingo <kenneth@thinkingmachin.es>
2024-02-09 01:49:56 +0800

    feat(email): sentry integration

--- email/src/index.ts
+++ email/src/index.ts
@@ -3,12 +3,24 @@ import { createElement } from "react";
 import { serve } from "@hono/node-server";
 import { zValidator } from "@hono/zod-validator";
 import { render } from "@react-email/render";
+import * as Sentry from "@sentry/node";
+import { ProfilingIntegration } from "@sentry/profiling-node";
 import { Hono } from "hono";
 import { bearerAuth } from "hono/bearer-auth";
 import { secureHeaders } from "hono/secure-headers";
 
 import DataQualityReport from "../emails/dq-report";
 import { DataQualityReportEmailProps } from "../types/dq-report";
+
+if (process.env.SENTRY_DSN) {
+  Sentry.init({
+    dsn: process.env.NODE_SENTRY_DSN,
+    environment: process.env.DEPLOY_ENV ?? "local",
+    integrations: [new ProfilingIntegration()],
+    tracesSampleRate: 1.0,
+    profilesSampleRate: 1.0,
+  });
+}
 
 const app = new Hono();
 
