cde45f61a09e2e75290206c3b7e95667c2d3c919
Kenneth V. Domingo <kenneth@thinkingmachin.es>
2024-02-15 16:02:24 +0800

    style: [PARTIAL] check file upload buttons

--- ui/src/routes/check-file-uploads/index.lazy.tsx
+++ ui/src/routes/check-file-uploads/index.lazy.tsx
@@ -1,10 +1,15 @@
 import { useMemo, useState } from "react";
 
+import { Add } from "@carbon/icons-react";
 import {
+  Button,
+  Column,
   DataTable,
   DataTableHeader,
+  Grid,
   Heading, // @ts-expect-error paginationNav has no typescript declaration yet
   PaginationNav,
+  Row,
   Section,
   Stack,
   Table,
@@ -19,7 +24,6 @@ import { useQuery } from "@tanstack/react-query";
 import { Link, createLazyFileRoute } from "@tanstack/react-router";
 
 import { useApi } from "@/api";
-import { UploadLink } from "@/components/common/UploadLink";
 import StatusIndicator from "@/components/upload/StatusIndicator";
 
 export const Route = createLazyFileRoute("/check-file-uploads/")({
@@ -27,142 +31,165 @@ export const Route = createLazyFileRoute("/check-file-uploads/")({
 });
 
 export default function FileUploads() {
-  {
-    const [currentPage, setCurrentPage] = useState<number>(0);
+  const [currentPage, setCurrentPage] = useState<number>(0);
 
-    const api = useApi();
+  const api = useApi();
 
-    const { data: files } = useQuery({
-      queryKey: ["files"],
-      queryFn: api.uploads.list_files,
-    });
+  const { data: files } = useQuery({
+    queryKey: ["files"],
+    queryFn: api.uploads.list_files,
+  });
 
-    const rows =
-      files?.data.map(file => {
-        const { uid, country, dataset, timestamp } = file;
-        const dateUploadedLocal = new Date(timestamp).toLocaleString();
-        return {
-          id: uid,
-          dateUploaded: dateUploadedLocal,
-          dataset: dataset,
-          country: country,
-          status: (
-            <div className="flex">
-              <StatusIndicator className="mr-1" type="info" />
-              Uploaded
-            </div>
-          ),
-          actions: (
-            <Link
-              to="/check-file-uploads/$uploadId"
-              params={{
-                uploadId: uid,
-              }}
-            >
-              View
-            </Link>
-          ),
-        };
-      }) ?? [];
-
-    const columns = useMemo<DataTableHeader[]>(
-      () => [
-        {
-          key: "id",
-          header: "Upload Id",
-        },
-        {
-          key: "dateUploaded",
-          header: "Date Uploaded",
-        },
-        {
-          key: "dataset",
-          header: "Dataset",
-        },
-        {
-          key: "country",
-          header: "Country",
-        },
-        {
-          key: "status",
-          header: "Status",
-        },
-        {
-          key: "actions",
-          header: "Actions",
-        },
-      ],
-      [],
-    );
-
-    const ROWS_PER_PAGE = 10;
-
-    const maxPages = Math.ceil(rows.length / ROWS_PER_PAGE);
-    const startIndex = currentPage * ROWS_PER_PAGE;
-    const endIndex = startIndex + ROWS_PER_PAGE;
-    const rowSlice = rows.slice(startIndex, endIndex);
-
-    return (
-      <Section className="container py-6">
-        <Stack gap={6}>
-          <Section>
-            <Heading>What will you be uploading today?</Heading>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur.
-            </p>
-          </Section>
-          <div className="flex gap-8">
-            <UploadLink uploadType="geolocation">Geolocation</UploadLink>
-            <UploadLink uploadType="coverage">Coverage</UploadLink>
+  const rows =
+    files?.data.map(file => {
+      const { uid, country, dataset, timestamp } = file;
+      const dateUploadedLocal = new Date(timestamp).toLocaleString();
+      return {
+        id: uid,
+        dateUploaded: dateUploadedLocal,
+        dataset: dataset,
+        country: country,
+        status: (
+          <div className="flex">
+            <StatusIndicator className="mr-1" type="info" />
+            Uploaded
           </div>
-          <Section>
-            <DataTable headers={columns} rows={rowSlice}>
-              {({
-                rows,
-                headers,
-                getHeaderProps,
-                getRowProps,
-                getTableProps,
-              }) => (
-                <TableContainer>
-                  <Table {...getTableProps()}>
-                    <TableHead>
-                      <TableRow>
-                        {headers.map(header => (
-                          // @ts-expect-error onclick bad type https://github.com/carbon-design-system/carbon/issues/14831
-                          <TableHeader {...getHeaderProps({ header })}>
-                            {header.header}
-                          </TableHeader>
+        ),
+        actions: (
+          <Link
+            to="/check-file-uploads/$uploadId"
+            params={{
+              uploadId: uid,
+            }}
+          >
+            View
+          </Link>
+        ),
+      };
+    }) ?? [];
+
+  const columns = useMemo<DataTableHeader[]>(
+    () => [
+      {
+        key: "id",
+        header: "Upload Id",
+      },
+      {
+        key: "dateUploaded",
+        header: "Date Uploaded",
+      },
+      {
+        key: "dataset",
+        header: "Dataset",
+      },
+      {
+        key: "country",
+        header: "Country",
+      },
+      {
+        key: "status",
+        header: "Status",
+      },
+      {
+        key: "actions",
+        header: "Actions",
+      },
+    ],
+    [],
+  );
+
+  const ROWS_PER_PAGE = 10;
+
+  const maxPages = Math.ceil(rows.length / ROWS_PER_PAGE);
+  const startIndex = currentPage * ROWS_PER_PAGE;
+  const endIndex = startIndex + ROWS_PER_PAGE;
+  const rowSlice = rows.slice(startIndex, endIndex);
+
+  return (
+    <Section className="container py-6">
+      <Stack gap={6}>
+        <Section>
+          <Heading>What will you be uploading today?</Heading>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+          </p>
+        </Section>
+        <Grid fullWidth>
+          <Row>
+            <Column>
+              <Button
+                as={Link}
+                to="/upload/$uploadGroup/$uploadType"
+                params={{
+                  uploadGroup: "school-data",
+                  uploadType: "geolocation",
+                }}
+                size="2xl"
+                renderIcon={Add}
+              >
+                School geolocation
+              </Button>
+            </Column>
+            <Column>
+              <Button
+                as={Link}
+                to="/upload/$uploadGroup/$uploadType"
+                params={{ uploadGroup: "school-data", uploadType: "coverage" }}
+                size="2xl"
+                renderIcon={Add}
+              >
+                School coverage
+              </Button>
+            </Column>
+          </Row>
+        </Grid>
+        <Section>
+          <DataTable headers={columns} rows={rowSlice}>
+            {({
+              rows,
+              headers,
+              getHeaderProps,
+              getRowProps,
+              getTableProps,
+            }) => (
+              <TableContainer>
+                <Table {...getTableProps()}>
+                  <TableHead>
+                    <TableRow>
+                      {headers.map(header => (
+                        // @ts-expect-error onclick bad type https://github.com/carbon-design-system/carbon/issues/14831
+                        <TableHeader {...getHeaderProps({ header })}>
+                          {header.header}
+                        </TableHeader>
+                      ))}
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {rows.map(row => (
+                      <TableRow {...getRowProps({ row })}>
+                        {row.cells.map(cell => (
+                          <TableCell key={cell.id}>{cell.value}</TableCell>
                         ))}
                       </TableRow>
-                    </TableHead>
-                    <TableBody>
-                      {rows.map(row => (
-                        <TableRow {...getRowProps({ row })}>
-                          {row.cells.map(cell => (
-                            <TableCell key={cell.id}>{cell.value}</TableCell>
-                          ))}
-                        </TableRow>
-                      ))}
-                    </TableBody>
-                  </Table>
-                  <PaginationNav
-                    className="pagination-nav-right"
-                    itemsShown={5}
-                    totalItems={maxPages}
-                    onChange={(index: number) => setCurrentPage(index)}
-                  />
-                </TableContainer>
-              )}
-            </DataTable>
-          </Section>
-        </Stack>
-      </Section>
-    );
-  }
+                    ))}
+                  </TableBody>
+                </Table>
+                <PaginationNav
+                  className="pagination-nav-right"
+                  itemsShown={5}
+                  totalItems={maxPages}
+                  onChange={(index: number) => setCurrentPage(index)}
+                />
+              </TableContainer>
+            )}
+          </DataTable>
+        </Section>
+      </Stack>
+    </Section>
+  );
 }
