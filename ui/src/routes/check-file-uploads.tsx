53dd6a13b504bf80750d8c32e1d3e2e379dfbbcb
GERLITO CHAGAS <ger@thinkingmachin.es>
2024-02-20 20:04:46 +0800

    fix: remove unused useffect

--- ui/src/routes/check-file-uploads.tsx
+++ ui/src/routes/check-file-uploads.tsx
@@ -1,24 +1,13 @@
-import { useEffect } from "react";
-
 import { AuthenticatedTemplate } from "@azure/msal-react";
 import { Outlet, createFileRoute } from "@tanstack/react-router";
 
 import CheckFileUploadsBreadCrumbs from "@/components/upload/CheckFileUploadsBreadCrumbs";
-import { useStore } from "@/store.ts";
 
 export const Route = createFileRoute("/check-file-uploads")({
   component: UploadLayout,
 });
 
 function UploadLayout() {
-  const { resetUploadState } = useStore();
-
-  useEffect(() => {
-    return () => {
-      resetUploadState();
-    };
-  }, [resetUploadState]);
-
   return (
     <AuthenticatedTemplate>
       <div className="container flex flex-col gap-4 py-6">
