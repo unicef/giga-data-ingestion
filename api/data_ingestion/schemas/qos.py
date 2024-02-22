36cc521b03a5c08254dec6f48f2844e6b4816eb2
GERLITO CHAGAS <ger@thinkingmachin.es>
2024-02-22 10:23:39 +0800

    feat: add School List schema

---
+++ api/data_ingestion/schemas/qos.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+
+from pydantic import BaseModel, ConfigDict, EmailStr
+
+from data_ingestion.models.school_list import (
+    AuthorizationTypeEnum,
+    PaginationTypeEnum,
+    RequestMethodEnum,
+    SendQueryInEnum,
+)
+
+
+class SchoolListSchema(BaseModel):
+    id: str
+    request_method: RequestMethodEnum
+    api_endpoint: str
+    data_key: str
+    school_id_key: str
+
+    query_parameters: str
+    request_body: str
+    authorization_type: AuthorizationTypeEnum
+
+    bearer_auth_bearer_token: str
+    basic_auth_username: str
+    basic_auth_password: str
+    api_auth_api_key: str
+    api_auth_api_value: str
+
+    pagination_type: PaginationTypeEnum
+    size: int
+    page_size_key: str
+    send_query_in: SendQueryInEnum
+
+    page_number_key: str
+    page_starts_with: int
+    page_offset_key: str
+    enabled: bool
+    date_created: datetime
+    date_modified: datetime
+    user_id: str
+    user_email: EmailStr
+
+    model_config = ConfigDict(from_attributes=True)
