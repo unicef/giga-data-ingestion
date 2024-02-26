14f105837796affd48949f31d2e3f7eda615bccb
Kenneth V. Domingo <kenneth@thinkingmachin.es>
2024-02-26 21:21:48 +0800

    refactor: landing page

--- ui/src/components/landing/Login.tsx
+++ ui/src/components/landing/Login.tsx
@@ -1,9 +1,25 @@
 import { useMsal } from "@azure/msal-react";
 import { ArrowRight, DocumentAdd } from "@carbon/icons-react";
-import { Button, Column, Grid, Heading } from "@carbon/react";
+import { Button, Heading } from "@carbon/react";
 import { Link } from "@tanstack/react-router";
 
 import { loginRequest } from "@/lib/auth.ts";
+
+const cards: { title: string; description: string }[] = [
+  {
+    title: "Upload data file",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+  },
+  {
+    title: "Configure API",
+    description:
+      "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+  },
+  {
+    title: "Check data quality",
+    description: "Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+  },
+];
 
 function Login() {
   const { instance } = useMsal();
@@ -17,9 +33,9 @@ function Login() {
   }
 
   return (
-    <div className="flex h-full w-full flex-col justify-center gap-6">
-      <Grid>
-        <Column lg={16}>
+    <div className="container flex h-full w-full flex-col justify-center gap-6">
+      <div>
+        <div>
           <Heading>
             giga<b>sync</b>
           </Heading>
@@ -27,54 +43,37 @@ function Login() {
             Upload and view quality datasets to help connect every school to the
             internet.
           </p>
-        </Column>
-      </Grid>
+        </div>
+      </div>
 
-      <Grid>
-        <Column lg={4}>
-          <div className="flex-col gap-4 bg-giga-light-gray p-4 text-black hover:bg-giga-light-gray hover:text-black">
-            <div className="text-xl">Upload data file</div>
-            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit</div>
-            <DocumentAdd size={60} />
+      <div className="grid grid-cols-4 gap-4">
+        {cards.map(card => (
+          <div className="flex flex-col gap-4 bg-giga-light-gray p-4 text-black hover:bg-giga-light-gray hover:text-black">
+            <div className="text-2xl font-semibold">{card.title}</div>
+            <div>{card.description}</div>
+            <DocumentAdd size={60} className="text-giga-dark-gray" />
           </div>
-        </Column>
-        <Column lg={4}>
-          <div className="flex-col gap-4 bg-giga-light-gray p-4 text-black hover:bg-giga-light-gray hover:text-black">
-            <div className="text-xl">Configure API</div>
-            <div>
-              Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
-            </div>
-            <DocumentAdd size={60} />
-          </div>
-        </Column>
-        <Column lg={4}>
-          <div className="flex-col gap-4 bg-giga-light-gray p-4 text-black hover:bg-giga-light-gray hover:text-black">
-            <div className="text-xl">Check data quality</div>
-            <div>
-              Ut enim ad minim veniam, quis nostrud exercitation ullamco
-            </div>
-            <DocumentAdd size={60} />
-          </div>
-        </Column>
-      </Grid>
+        ))}
+      </div>
 
-      <Grid>
-        <Column lg={4}>
-          <Button
-            className="w-full"
-            renderIcon={ArrowRight}
-            onClick={handleLogin}
-          >
-            Log in
-          </Button>
-          <div>
-            Login lorem ipsum{" "}
-            <Link to="/">
-              <u>Dolor sit Amet</u>
-            </Link>
-          </div>
-        </Column>
-      </Grid>
+      <div />
+
+      <div className="flex flex-col gap-2">
+        <Button
+          className="w-full"
+          renderIcon={ArrowRight}
+          onClick={handleLogin}
+          isExpressive
+        >
+          Log in
+        </Button>
+        <div>
+          Login lorem ipsum{" "}
+          <Link to="/">
+            <u>Dolor sit Amet</u>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
