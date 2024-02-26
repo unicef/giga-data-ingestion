d65f9d67a7ce1d2e6867e6b8210706ded36c9d35
Kenneth V. Domingo <kenneth@thinkingmachin.es>
2024-02-26 20:45:45 +0800

    refactor: upload page

--- ui/src/routes/index.tsx
+++ ui/src/routes/index.tsx
@@ -1,11 +1,10 @@
 import { useEffect } from "react";
 
-import { Add } from "@carbon/icons-react";
-import { Button, Column, Grid, Heading, Section, Stack } from "@carbon/react";
-import { Link, createFileRoute } from "@tanstack/react-router";
+import { Column, Grid, Stack } from "@carbon/react";
+import { createFileRoute } from "@tanstack/react-router";
 
-import UploadsTable from "@/components/check-file-uploads/UploadsTable.tsx";
 import UploadBreadcrumbs from "@/components/upload/UploadBreadcrumbs.tsx";
+import UploadLanding from "@/components/upload/UploadLanding.tsx";
 import AuthenticatedRBACView from "@/components/utils/AuthenticatedRBACView.tsx";
 import { useStore } from "@/store.ts";
 
@@ -28,70 +27,7 @@ function Index() {
         <Column lg={16} className="py-6">
           <Stack gap={6}>
             <UploadBreadcrumbs />
-
-            <Section>
-              <Section>
-                <Stack gap={4}>
-                  <Heading>What will you be uploading today?</Heading>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                  </p>
-                  <Grid>
-                    <Column lg={4}>
-                      <Button
-                        as={Link}
-                        to="/upload/$uploadGroup/$uploadType"
-                        params={{
-                          uploadGroup: "school-data",
-                          uploadType: "geolocation",
-                        }}
-                        className="w-full"
-                        size="xl"
-                        renderIcon={Add}
-                      >
-                        School geolocation
-                      </Button>
-                    </Column>
-                    <Column lg={4}>
-                      <Button
-                        as={Link}
-                        to="/upload/$uploadGroup/$uploadType"
-                        params={{
-                          uploadGroup: "school-data",
-                          uploadType: "coverage",
-                        }}
-                        className="w-full"
-                        size="xl"
-                        renderIcon={Add}
-                      >
-                        School coverage
-                      </Button>
-                    </Column>
-                    {/* TODO: Scope this out */}
-                    {/*<Column lg={4}>*/}
-                    {/*  <Button*/}
-                    {/*    as={Link}*/}
-                    {/*    to="/upload/$uploadGroup/$uploadType"*/}
-                    {/*    params={{*/}
-                    {/*      uploadGroup: "other",*/}
-                    {/*      uploadType: "unstructured",*/}
-                    {/*    }}*/}
-                    {/*    className="w-full"*/}
-                    {/*    size="xl"*/}
-                    {/*    renderIcon={Add}*/}
-                    {/*  >*/}
-                    {/*    Unstructured dataset*/}
-                    {/*  </Button>*/}
-                    {/*</Column>*/}
-                  </Grid>
-
-                  <UploadsTable />
-                </Stack>
-              </Section>
-            </Section>
+            <UploadLanding />
           </Stack>
         </Column>
       </Grid>
--- ui/src/routes/upload/index.lazy.tsx
+++ ui/src/routes/upload/index.lazy.tsx
@@ -1,75 +1,7 @@
-import { Add } from "@carbon/icons-react";
-import { Button, Column, Grid, Heading, Section, Stack } from "@carbon/react";
-import { Link, createLazyFileRoute } from "@tanstack/react-router";
+import { createLazyFileRoute } from "@tanstack/react-router";
 
-import UploadsTable from "@/components/check-file-uploads/UploadsTable.tsx";
+import UploadLanding from "@/components/upload/UploadLanding.tsx";
 
 export const Route = createLazyFileRoute("/upload/")({
-  component: () => (
-    <Section>
-      <Section>
-        <Stack gap={4}>
-          <Heading>What will you be uploading today?</Heading>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </p>
-          <Grid>
-            <Column lg={4}>
-              <Button
-                as={Link}
-                to="/upload/$uploadGroup/$uploadType"
-                params={{
-                  uploadGroup: "school-data",
-                  uploadType: "geolocation",
-                }}
-                className="w-full"
-                size="xl"
-                renderIcon={Add}
-                isExpressive
-              >
-                School geolocation
-              </Button>
-            </Column>
-            <Column lg={4}>
-              <Button
-                as={Link}
-                to="/upload/$uploadGroup/$uploadType"
-                params={{
-                  uploadGroup: "school-data",
-                  uploadType: "coverage",
-                }}
-                className="w-full"
-                size="xl"
-                renderIcon={Add}
-                isExpressive
-              >
-                School coverage
-              </Button>
-            </Column>
-            {/* TODO: Scope this out */}
-            {/*<Column lg={4}>*/}
-            {/*  <Button*/}
-            {/*    as={Link}*/}
-            {/*    to="/upload/$uploadGroup/$uploadType"*/}
-            {/*    params={{*/}
-            {/*      uploadGroup: "other",*/}
-            {/*      uploadType: "unstructured",*/}
-            {/*    }}*/}
-            {/*    className="w-full"*/}
-            {/*    size="xl"*/}
-            {/*    renderIcon={Add}*/}
-            {/*  >*/}
-            {/*    Unstructured dataset*/}
-            {/*  </Button>*/}
-            {/*</Column>*/}
-          </Grid>
-
-          <UploadsTable />
-        </Stack>
-      </Section>
-    </Section>
-  ),
+  component: UploadLanding,
 });
