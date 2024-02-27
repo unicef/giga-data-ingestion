e2caeac1741456f45c13bade0844c713371b4acd
GERLITO CHAGAS <ger@thinkingmachin.es>
2024-02-27 11:20:40 +0800

    feat: add controlled number input for school list

---
+++ ui/src/components/upload/ControllerNumberInputSchoolList.tsx
@@ -0,0 +1,38 @@
+import { ComponentProps } from "react";
+import { Control, FieldPath, useController } from "react-hook-form";
+
+import { NumberInput } from "@carbon/react";
+
+import { SchoolListFormValues } from "@/types/qos";
+
+type NumberInputProps = ComponentProps<typeof NumberInput>;
+
+interface ControlledDatepickerProps {
+  control: Control<SchoolListFormValues>;
+  name: FieldPath<SchoolListFormValues>;
+  numberInputProps: NumberInputProps;
+}
+
+const ControllerNumberInputSchoolList = ({
+  control,
+  name,
+  numberInputProps,
+}: ControlledDatepickerProps) => {
+  const { field, fieldState } = useController({
+    name,
+    control,
+    rules: { required: true },
+  });
+
+  return (
+    <NumberInput
+      invalid={fieldState.invalid}
+      onChange={(_, value) => {
+        field.onChange(value.value);
+      }}
+      {...numberInputProps}
+    />
+  );
+};
+
+export default ControllerNumberInputSchoolList;
