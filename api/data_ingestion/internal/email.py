e4868a973b40b5088511126c302fb911607a6037
GERLITO CHAGAS <ger@thinkingmachin.es>
2024-02-27 19:38:02 +0800

    refactor: make send email reusable function

--- api/data_ingestion/internal/email.py
+++ api/data_ingestion/internal/email.py
@@ -7,16 +7,21 @@ from data_ingestion.schemas.invitation import InviteEmailRenderRequest
 from data_ingestion.settings import settings
 
 
-def invite_user(body: InviteEmailRenderRequest):
+def send_email_base(
+    endpoint: str,
+    json: dict[str, Any],
+    recepient: str,
+    subject: str,
+):
     client = EmailClient.from_connection_string(settings.AZURE_EMAIL_CONNECTION_STRING)
 
     res = requests.post(
-        f"{settings.EMAIL_RENDERER_SERVICE_URL}email/invite-user",
+        f"{settings.EMAIL_RENDERER_SERVICE_URL}{endpoint}",
         headers={
             "Content-Type": "application/json",
-            "Authorization": f"Bearer {settings.EMAIL_RENDERER_BEARER_TOKEN}",
+            "Authorization": f"Bearer {settings.EMAIL_RENDERER_BEARER_TOKEN}",  #
         },
-        json=body.model_dump(),
+        json=json,
     )
     if not res.ok:
         try:
@@ -28,13 +33,24 @@ def invite_user(body: InviteEmailRenderRequest):
 
     message = {
         "senderAddress": settings.AZURE_EMAIL_SENDER,
-        "recipients": {"to": [{"address": body.email}]},
+        "recipients": {"to": [{"address": recepient}]},
         "content": {
-            "subject": "Welcome to Giga Sync",
+            "subject": subject,
             "html": data.get("html"),
             "plainText": data.get("text"),
         },
     }
+
     poller = client.begin_send(message)
     result = poller.result()
     logger.info(result)
+
+
+def invite_user(body: InviteEmailRenderRequest):
+    send_email_base(
+        endpoint="email/invite-user",
+        json=body.model_dump(),
+        recepient=body.email,
+        subject="Welcome to Giga Sync",
+    )
+
