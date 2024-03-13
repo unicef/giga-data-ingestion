17c5c2ea5e5e511d4857a0d479d2ca5125f6a5b3
GERLITO CHAGAS <ger@thinkingmachin.es>
2024-03-13 09:46:00 +0800

    fix: update interface to not use outdated naming convention

--- email/src/types/dq-report.ts
+++ email/src/types/dq-report.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-export interface IDataQualityUploadSuccessProps {
+export interface DataQualityUploadSuccessProps {
   uploadId: string;
   dataset: string;
   uploadDate: string;
