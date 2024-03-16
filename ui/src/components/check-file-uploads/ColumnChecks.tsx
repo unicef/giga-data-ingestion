8fe0cfa82ae8ab65781e74c80d32e0107cdc15ff
GERLITO CHAGAS <ger@thinkingmachin.es>
2024-03-16 15:37:37 +0800

    feat: add description tooltips to column check columns

--- ui/src/components/check-file-uploads/ColumnChecks.tsx
+++ ui/src/components/check-file-uploads/ColumnChecks.tsx
@@ -1,174 +1,216 @@
 import { useState } from "react";
 
-import { Information } from "@carbon/icons-react";
-import { DataTableSkeleton, Link, Modal, Tooltip } from "@carbon/react";
+import {
+  DataTable,
+  DataTableHeader,
+  DefinitionTooltip,
+  Link, // @ts-expect-error paginationNav has no typescript declaration yet
+  PaginationNav,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@carbon/react";
 
-import PaginatedDatatable from "@/components/upload/PaginatedDatatable";
-import { columnChecksHeaders } from "@/constants/check-file-uploads";
-import { columnCheckModalHeaders } from "@/constants/check-file-uploads";
-import { DataQualityCheckResult } from "@/types/upload";
-import { ColumnCheck } from "@/types/upload";
+import { MasterSchema, masterSchemaData } from "@/constants/school-data";
+import { DataRelevanceEnum } from "@/constants/school-data";
+import {
+  Check,
+  DqFailedRowValues,
+  DqFailedRowsFirstFiveRows,
+} from "@/types/upload";
 
-import Datatable from "../upload/Datatable";
 import StatusIndicator from "../upload/StatusIndicator";
+import ViewDetailsModal from "./ViewDetailsModal";
 
-type ColumnCheckRow = {
-  id: string;
-  value: string;
-  count: number;
-}[];
+const dqResultHeaders: DataTableHeader[] = [
+  { key: "column", header: "Column Name" },
+  { key: "assertion", header: "Assertion" },
+  { key: "count_failed", header: "Count Failed" },
+  { key: "percent_failed", header: "Percent Failed" },
+  { key: "count_passed", header: "Count Passed" },
+  { key: "percent_passed", header: "Percent Passed" },
+  { key: "actions", header: "Actions" },
+];
 
-export default function ColumnChecks({
-  data: data,
-  isLoading: isLoading,
-}: {
-  data: DataQualityCheckResult | undefined;
-  isLoading: boolean;
-}) {
-  const [isColumnChecksModalOpen, setIsColumnChecksModalOpen] =
-    useState<boolean>(false);
-  const [columnCheckModalRows, setColumnCheckModalRows] =
-    useState<ColumnCheckRow>([]);
-  const [selectedColumnCheck, setSelectedColumnCheck] = useState<ColumnCheck>({
-    assertion: "",
-    description: "",
-    data_type: "",
-    is_present: false,
-    is_correct_datatype: false,
-    null_values_count: 0,
-    unique_values_count: 0,
-    unique_values: [],
-    rows_failed: [],
-  });
-
-  if (data === undefined) return <></>;
-
-  const columnChecksRows = data.column_checks.map(check => {
-    return {
-      id: check.assertion,
-      columnName: (
-        <Tooltip
-          align="right"
-          enterDelayMs={150}
-          label={`${check.description}`}
-          leaveDelayMs={0}
-        >
-          <div className="flex gap-1">
-            {check.assertion}
-            <div className="flex items-center opacity-25">
-              <Information />
-            </div>
-          </div>
-        </Tooltip>
-      ),
-      expectedDataType: check.data_type,
-      isPresent: (
-        <div className="flex">
-          <StatusIndicator
-            className="mr-1"
-            type={check.is_present ? "success" : "error"}
-          />
-          Present
-        </div>
-      ),
-      isCorrectDatatype: (
-        <div className="flex">
-          <StatusIndicator
-            className="mr-1"
-            type={check.is_correct_datatype ? "success" : "error"}
-          />
-          {check.data_type}
-        </div>
-      ),
-      nullValuesCount: (
-        <div className="flex">
-          <StatusIndicator
-            className="mr-1"
-            type={check.null_values_count == 0 ? "success" : "error"}
-          />
-          {check.null_values_count} null values
-        </div>
-      ),
-      uniqueValuesCount: (
-        <div className="flex">
-          <StatusIndicator className="mr-1" type="info" />
-          {check.unique_values_count} unique values
-        </div>
-      ),
-      actions: (
-        <Link
-          onClick={() => {
-            setIsColumnChecksModalOpen(true);
-
-            const rows = check.unique_values.map(row => {
-              return {
-                id: row.name,
-                value: row.name,
-                count: row.count,
-              };
-            });
-
-            setSelectedColumnCheck(check);
-            setColumnCheckModalRows(rows);
-          }}
-        >
-          View Details
-        </Link>
-      ),
-    };
-  });
-
-  const columnCheckPassedRows = data.column_checks.reduce(
-    (acc, curr) =>
-      curr.is_present === true &&
-      curr.is_correct_datatype === true &&
-      curr.null_values_count === 0
-        ? acc + 1
-        : acc,
-    0,
-  );
-
-  const columnCheckFailedRows = data.column_checks.reduce(
-    (acc, curr) =>
-      curr.is_present !== true ||
-      curr.is_correct_datatype !== true ||
-      curr.null_values_count !== 0
-        ? acc + 1
-        : acc,
-    0,
-  );
-
-  if (isLoading)
-    return <DataTableSkeleton headers={columnCheckModalHeaders ?? []} />;
-
-  return (
-    <>
-      <div className="py-4">
-        Out of {data.column_checks.length} columns,{" "}
-        <b>{columnCheckPassedRows}</b> passed and had{" "}
-        <b>{columnCheckFailedRows}</b> errors
-      </div>
-      <PaginatedDatatable
-        headers={columnChecksHeaders}
-        rows={columnChecksRows ?? []}
-      />
-
-      <Modal
-        modalHeading="Unique Values Check"
-        open={isColumnChecksModalOpen}
-        passiveModal
-        onRequestClose={() => setIsColumnChecksModalOpen(false)}
-      >
-        <p>
-          {" "}
-          There are <b>{selectedColumnCheck.unique_values_count}</b> unique
-          values in <b>{selectedColumnCheck.assertion}</b>
-        </p>
-        <Datatable
-          headers={columnCheckModalHeaders ?? []}
-          rows={columnCheckModalRows ?? []}
-        />
-      </Modal>
-    </>
-  );
+interface DataQualityChecksProps {
+  data: Check[];
+  previewData: DqFailedRowsFirstFiveRows;
 }
+
+const ITEMS_PER_PAGE = 10;
+const DataQualityChecks = ({ data, previewData }: DataQualityChecksProps) => {
+  const [page, setPage] = useState(0);
+  const [selectedAssertion, setSelctedAssertion] = useState<string>("");
+  const [selectedColumn, setSelectedColumn] = useState<string>("");
+  const [selectedPreviewData, setSelectedPreviewData] = useState<
+    DqFailedRowValues[]
+  >([{}]);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  const masterSchemaDataWithMissingFields = {
+    ...masterSchemaData,
+    admin1_id_giga: {
+      description: "NO_DESCRIPTION",
+      data_relevance: DataRelevanceEnum.Optional,
+    },
+    admin2_id_giga: {
+      description: "NO_DESCRIPTION",
+      data_relevance: DataRelevanceEnum.Optional,
+    },
+    nearest_NR_distance: {
+      description: "NO_DESCRIPTION",
+      data_relevance: DataRelevanceEnum.Optional,
+    },
+    nearest_NR_id: {
+      description: "NO_DESCRIPTION",
+      data_relevance: DataRelevanceEnum.Optional,
+    },
+  };
+
+  const rows = data
+    .sort((a, b) => {
+      if (a.column < b.column) {
+        return -1;
+      }
+      if (a.column > b.column) {
+        return 1;
+      }
+      return 0;
+    })
+    .map(check => {
+      const {
+        assertion,
+        column,
+        description,
+        count_failed,
+        count_passed,
+        count_overall,
+        percent_failed,
+        percent_passed,
+      } = check;
+
+      return {
+        id: `${assertion}-${column}`,
+        column: (
+          <div className="min-w-64">
+            <DefinitionTooltip
+              align="right"
+              definition={
+                masterSchemaDataWithMissingFields[column as keyof MasterSchema]
+                  .description
+              }
+              openOnHover
+            >
+              {column}
+            </DefinitionTooltip>
+          </div>
+        ),
+        assertion: (
+          <div className="min-w-64 ">
+            <DefinitionTooltip
+              align="right"
+              definition={description}
+              openOnHover
+            >
+              {assertion}
+            </DefinitionTooltip>
+          </div>
+        ),
+        count_failed: (
+          <div className="flex">
+            {count_failed}/{count_overall}
+          </div>
+        ),
+        percent_failed: (
+          <div className="flex">
+            <StatusIndicator className="mr-1" type="error" />
+            {percent_failed}%
+          </div>
+        ),
+        count_passed: (
+          <div className="flex">
+            {count_passed}/{count_overall}
+          </div>
+        ),
+        percent_passed: (
+          <div className="flex">
+            <StatusIndicator className="mr-1" type="success" />
+            {percent_passed}%
+          </div>
+        ),
+        actions: (
+          <Link
+            className="cursor-pointer"
+            onClick={() => {
+              const dfasdf = previewData[`${assertion}-${column}`];
+
+              setSelctedAssertion(assertion);
+              setSelectedPreviewData(dfasdf);
+              setIsModalOpen(true);
+              setSelectedColumn(column);
+            }}
+          >
+            View Details
+          </Link>
+        ),
+      };
+    });
+
+  const maxPages = Math.ceil(data.length / ITEMS_PER_PAGE);
+  const startIndex = page * ITEMS_PER_PAGE;
+  const endIndex = startIndex + ITEMS_PER_PAGE;
+  const rowSlice = rows.slice(startIndex, endIndex);
+  return (
+    <div>
+      <DataTable headers={dqResultHeaders} rows={rowSlice}>
+        {({ rows, headers, getHeaderProps, getRowProps, getTableProps }) => (
+          <TableContainer>
+            <Table {...getTableProps()}>
+              <TableHead>
+                <TableRow>
+                  {headers.map(header => (
+                    // @ts-expect-error onclick bad type https://github.com/carbon-design-system/carbon/issues/14831
+                    <TableHeader
+                      className="bg-blue-200  "
+                      colSpan={1}
+                      {...getHeaderProps({ header })}
+                    >
+                      {header.header}
+                    </TableHeader>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows.map(row => (
+                  <TableRow {...getRowProps({ row })}>
+                    {row.cells.map(cell => (
+                      <TableCell key={cell.id}>{cell.value}</TableCell>
+                    ))}
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+            <PaginationNav
+              itemsShown={ITEMS_PER_PAGE}
+              totalItems={maxPages}
+              onChange={(index: number) => setPage(index)}
+            />
+          </TableContainer>
+        )}
+      </DataTable>
+      <ViewDetailsModal
+        assertion={selectedAssertion}
+        column={selectedColumn}
+        previewData={selectedPreviewData}
+        open={isModalOpen}
+        setOpen={setIsModalOpen}
+      />
+    </div>
+  );
+};
+
+export default DataQualityChecks;
