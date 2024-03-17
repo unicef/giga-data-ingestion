9b81d41ada5a06bb7a550e72894ccff7451139aa
Kenneth V. Domingo <kenneth@thinkingmachin.es>
2024-03-18 00:04:26 +0800

    chore: restore auth on schemas path

--- api/data_ingestion/routers/schema.py
+++ api/data_ingestion/routers/schema.py
@@ -1,14 +1,15 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, Security, status
 from sqlalchemy import text
 from sqlalchemy.orm import Session
 
 from data_ingestion.db.trino import get_db
+from data_ingestion.internal.auth import azure_scheme
 from data_ingestion.schemas.schema import Schema as MetaSchema
 
 router = APIRouter(
     prefix="/api/schema",
     tags=["schema"],
-    # dependencies=[Security(azure_scheme)],
+    dependencies=[Security(azure_scheme)],
 )
 
 
@@ -23,6 +24,7 @@ async def get_schema(name: str, db: Session = Depends(get_db)):
     ]:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
 
+    # TODO: Determine how to dynamically pass table names with ORM
     res = db.execute(
         text(
             f"SELECT * FROM schemas.{name} ORDER BY primary_key, is_nullable NULLS LAST"
