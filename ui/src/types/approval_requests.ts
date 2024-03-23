7955c8813a9b0d0326c5370cf7a4491848955676
GERLITO CHAGAS <ger@thinkingmachin.es>
2024-03-23 19:09:45 +0800

    feat: add approval rows util and types

---
+++ ui/src/types/approval_requests.ts
@@ -0,0 +1,25 @@
+export interface ApprovalRequestListing {
+  country: string;
+  country_iso3: string;
+  dataset: string;
+  subpath: string;
+  last_modified: string;
+  rows_count: number;
+  rows_added: number;
+  rows_updated: number;
+  rows_deleted: number;
+}
+
+interface ApprovalRequestInfo {
+  country: string;
+  dataset: string;
+}
+
+export type ApprovalRequestData = Record<string, null> & {
+  _change_type: "insert" | "remove" | "update_preimage";
+};
+
+export type ApprovalRequest = {
+  info: ApprovalRequestInfo;
+  data: ApprovalRequestData[];
+};
