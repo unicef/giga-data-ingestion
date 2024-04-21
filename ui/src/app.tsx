ee39e3986eba27e494585fba2fee11e8aa8e5fa1
Kenneth V. Domingo <kenneth@thinkingmachin.es>
2024-04-22 05:14:05 +0800

    Revert "fix(ui): make msal init non-blocking"
    This reverts commit d35e560a443cfeab1822d70280c51206abf21bd3.

--- ui/src/app.tsx
+++ ui/src/app.tsx
@@ -1,17 +1,6 @@
-import React, { useEffect } from "react";
-import { HelmetProvider } from "react-helmet-async";
-
-import {
-  AuthenticationResult,
-  EventMessage,
-  EventType,
-} from "@azure/msal-browser";
-import { MsalProvider } from "@azure/msal-react";
-import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 
-import { AxiosProvider, queryClient } from "@/api";
-import { msalInstance } from "@/lib/auth.ts";
+import { queryClient } from "@/api";
 import { routeTree } from "@/routeTree.gen.ts";
 
 const router = createRouter({
@@ -29,37 +18,8 @@ declare module "@tanstack/react-router" {
   }
 }
 
-export default function App() {
-  useEffect(() => {
-    msalInstance
-      .initialize()
-      .then(() => {
-        const accounts = msalInstance.getAllAccounts();
-        if (accounts.length > 0) {
-          msalInstance.setActiveAccount(accounts[0]);
-        }
-
-        msalInstance.addEventCallback((event: EventMessage) => {
-          if (event.eventType === EventType.LOGIN_SUCCESS && event.payload) {
-            const { account } = event.payload as AuthenticationResult;
-            msalInstance.setActiveAccount(account);
-          }
-        });
-      })
-      .catch(console.error);
-  }, []);
-
-  return (
-    <React.StrictMode>
-      <MsalProvider instance={msalInstance}>
-        <AxiosProvider>
-          <QueryClientProvider client={queryClient}>
-            <HelmetProvider>
-              <RouterProvider router={router} />
-            </HelmetProvider>
-          </QueryClientProvider>
-        </AxiosProvider>
-      </MsalProvider>
-    </React.StrictMode>
-  );
+function App() {
+  return <RouterProvider router={router} />;
 }
+
+export default App;
--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,25 +1,32 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
 
+import {
+  AuthenticationResult,
+  EventMessage,
+  EventType,
+} from "@azure/msal-browser";
+import { MsalProvider } from "@azure/msal-react";
 import "@fontsource/open-sans/300.css";
 import "@fontsource/open-sans/400.css";
 import "@fontsource/open-sans/500.css";
 import "@fontsource/open-sans/600.css";
 import "@fontsource/open-sans/700.css";
-import {
-  browserTracingIntegration,
-  replayIntegration,
-  init as sentryInit,
-} from "@sentry/react";
+import * as Sentry from "@sentry/react";
+import { QueryClientProvider } from "@tanstack/react-query";
 
+import { AxiosProvider, queryClient } from "@/api";
 import App from "@/app.tsx";
+import { msalInstance } from "@/lib/auth.ts";
 import "@/styles/index.scss";
 
 if (import.meta.env.SENTRY_DSN && import.meta.env.PROD) {
-  sentryInit({
+  Sentry.init({
     dsn: import.meta.env.VITE_SENTRY_DSN,
     integrations: [
-      browserTracingIntegration(),
-      replayIntegration({ maskAllText: false, blockAllMedia: false }),
+      Sentry.browserTracingIntegration(),
+      Sentry.replayIntegration({ maskAllText: false, blockAllMedia: false }),
     ],
     sampleRate: 1.0,
     enableTracing: true,
@@ -34,4 +41,33 @@ if (import.meta.env.SENTRY_DSN && import.meta.env.PROD) {
   });
 }
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+msalInstance
+  .initialize()
+  .then(() => {
+    const accounts = msalInstance.getAllAccounts();
+    if (accounts.length > 0) {
+      msalInstance.setActiveAccount(accounts[0]);
+    }
+
+    msalInstance.addEventCallback((event: EventMessage) => {
+      if (event.eventType === EventType.LOGIN_SUCCESS && event.payload) {
+        const { account } = event.payload as AuthenticationResult;
+        msalInstance.setActiveAccount(account);
+      }
+    });
+
+    ReactDOM.createRoot(document.getElementById("root")!).render(
+      <React.StrictMode>
+        <MsalProvider instance={msalInstance}>
+          <AxiosProvider>
+            <QueryClientProvider client={queryClient}>
+              <HelmetProvider>
+                <App />
+              </HelmetProvider>
+            </QueryClientProvider>
+          </AxiosProvider>
+        </MsalProvider>
+      </React.StrictMode>,
+    );
+  })
+  .catch(console.error);
