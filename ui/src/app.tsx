d35e560a443cfeab1822d70280c51206abf21bd3
Kenneth V. Domingo <kenneth@thinkingmachin.es>
2024-04-22 01:56:17 +0800

    fix(ui): make msal init non-blocking

--- ui/src/app.tsx
+++ ui/src/app.tsx
@@ -1,6 +1,17 @@
+import React, { useEffect } from "react";
+import { HelmetProvider } from "react-helmet-async";
+
+import {
+  AuthenticationResult,
+  EventMessage,
+  EventType,
+} from "@azure/msal-browser";
+import { MsalProvider } from "@azure/msal-react";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 
-import { queryClient } from "@/api";
+import { AxiosProvider, queryClient } from "@/api";
+import { msalInstance } from "@/lib/auth.ts";
 import { routeTree } from "@/routeTree.gen.ts";
 
 const router = createRouter({
@@ -18,8 +29,37 @@ declare module "@tanstack/react-router" {
   }
 }
 
-function App() {
-  return <RouterProvider router={router} />;
-}
+export default function App() {
+  useEffect(() => {
+    msalInstance
+      .initialize()
+      .then(() => {
+        const accounts = msalInstance.getAllAccounts();
+        if (accounts.length > 0) {
+          msalInstance.setActiveAccount(accounts[0]);
+        }
 
-export default App;
+        msalInstance.addEventCallback((event: EventMessage) => {
+          if (event.eventType === EventType.LOGIN_SUCCESS && event.payload) {
+            const { account } = event.payload as AuthenticationResult;
+            msalInstance.setActiveAccount(account);
+          }
+        });
+      })
+      .catch(console.error);
+  }, []);
+
+  return (
+    <React.StrictMode>
+      <MsalProvider instance={msalInstance}>
+        <AxiosProvider>
+          <QueryClientProvider client={queryClient}>
+            <HelmetProvider>
+              <RouterProvider router={router} />
+            </HelmetProvider>
+          </QueryClientProvider>
+        </AxiosProvider>
+      </MsalProvider>
+    </React.StrictMode>
+  );
+}
