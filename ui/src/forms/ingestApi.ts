df2268ea4cb4be3448c6117360bd0b3ff4723c65
Kenneth V. Domingo <kenneth@thinkingmachin.es>
2024-04-22 00:27:59 +0800

    refactor(ui): extract validators

--- ui/src/forms/ingestApi.ts
+++ ui/src/forms/ingestApi.ts
@@ -2,6 +2,10 @@ import { z } from "zod";
 import { zu } from "zod_utilz";
 
 import {
+  validateAuthType,
+  validatePaginationType,
+} from "@/forms/validators.ts";
+import {
   AuthorizationTypeEnum,
   PaginationTypeEnum,
   RequestMethodEnum,
@@ -41,119 +45,8 @@ function commonSuperRefine(
   val: CommonApiIngestionFormSchema,
   ctx: z.RefinementCtx,
 ) {
-  switch (val.authorization_type) {
-    case AuthorizationTypeEnum.API_KEY: {
-      if (!val.api_auth_api_key) {
-        ctx.addIssue({
-          code: z.ZodIssueCode.custom,
-          message:
-            "API Key Name is required when Authorization Type is API Key",
-          path: ["api_auth_api_key"],
-        });
-      }
-
-      if (!val.api_auth_api_value) {
-        ctx.addIssue({
-          code: z.ZodIssueCode.custom,
-          message:
-            "API Key Value is required when Authorization Type is API KEY",
-          path: ["api_auth_api_value"],
-        });
-      }
-      break;
-    }
-    case AuthorizationTypeEnum.BEARER_TOKEN: {
-      if (!val.bearer_auth_bearer_token) {
-        ctx.addIssue({
-          code: z.ZodIssueCode.custom,
-          message:
-            "Bearer Token is required when Authorization Type is BEARER TOKEN",
-          path: ["bearer_auth_bearer_token"],
-        });
-      }
-      break;
-    }
-    case AuthorizationTypeEnum.BASIC_AUTH: {
-      if (!val.basic_auth_username) {
-        ctx.addIssue({
-          code: z.ZodIssueCode.custom,
-          message: "Username is required when Authorization Type is BASIC AUTH",
-          path: ["basic_auth_username"],
-        });
-      }
-      if (!val.basic_auth_password) {
-        ctx.addIssue({
-          code: z.ZodIssueCode.custom,
-          message: "Password is required when Authorization Type is BASIC AUTH",
-          path: ["basic_auth_password"],
-        });
-      }
-      break;
-    }
-  }
-
-  switch (val.pagination_type) {
-    case PaginationTypeEnum.LIMIT_OFFSET: {
-      if (!val.size || val.size < 1) {
-        ctx.addIssue({
-          code: z.ZodIssueCode.custom,
-          message: "Size is required when Pagination Method is LIMIT OFFSET",
-          path: ["size"],
-        });
-      }
-      if (!val.page_size_key) {
-        ctx.addIssue({
-          code: z.ZodIssueCode.custom,
-          message:
-            "Page Size Key is required when Pagination Method is LIMIT OFFSET",
-          path: ["page_size_key"],
-        });
-      }
-      if (!val.page_offset_key) {
-        ctx.addIssue({
-          code: z.ZodIssueCode.custom,
-          message:
-            "Page Offset Key is required when Pagination Method is LIMIT OFFSET",
-          path: ["page_offset_key"],
-        });
-      }
-      break;
-    }
-    case PaginationTypeEnum.PAGE_NUMBER: {
-      if (!val.size || val.size < 1) {
-        ctx.addIssue({
-          code: z.ZodIssueCode.custom,
-          message: "Size is required when Pagination Method is PAGE NUMBER",
-          path: ["size"],
-        });
-      }
-      if (!val.page_size_key) {
-        ctx.addIssue({
-          code: z.ZodIssueCode.custom,
-          message:
-            "Page Size Key is required when Pagination Method is PAGE NUMBER",
-          path: ["page_size_key"],
-        });
-      }
-      if (!val.page_number_key) {
-        ctx.addIssue({
-          code: z.ZodIssueCode.custom,
-          message:
-            "Page Number Key is required when Pagination Method is PAGE NUMBER",
-          path: ["page_number_key"],
-        });
-      }
-      if (!val.page_starts_with || val.page_starts_with < 0) {
-        ctx.addIssue({
-          code: z.ZodIssueCode.custom,
-          message:
-            "Page Starts With must be greater than or equal to 0 when Pagination Method is PAGE NUMBER",
-          path: ["page_starts_with"],
-        });
-      }
-      break;
-    }
-  }
+  validateAuthType(val, ctx);
+  validatePaginationType(val, ctx);
 
   if (!!val.school_id_key && !val.school_id_send_query_in) {
     ctx.addIssue({
---
+++ ui/src/forms/validators.ts
@@ -0,0 +1,128 @@
+import { z } from "zod";
+
+import { CommonApiIngestionFormSchema } from "@/forms/ingestApi.ts";
+import { AuthorizationTypeEnum, PaginationTypeEnum } from "@/types/qos.ts";
+
+export function validateAuthType(
+  val: CommonApiIngestionFormSchema,
+  ctx: z.RefinementCtx,
+) {
+  switch (val.authorization_type) {
+    case AuthorizationTypeEnum.API_KEY: {
+      if (!val.api_auth_api_key) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message:
+            "API Key Name is required when Authorization Type is API Key",
+          path: ["api_auth_api_key"],
+        });
+      }
+
+      if (!val.api_auth_api_value) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message:
+            "API Key Value is required when Authorization Type is API KEY",
+          path: ["api_auth_api_value"],
+        });
+      }
+      break;
+    }
+    case AuthorizationTypeEnum.BEARER_TOKEN: {
+      if (!val.bearer_auth_bearer_token) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message:
+            "Bearer Token is required when Authorization Type is BEARER TOKEN",
+          path: ["bearer_auth_bearer_token"],
+        });
+      }
+      break;
+    }
+    case AuthorizationTypeEnum.BASIC_AUTH: {
+      if (!val.basic_auth_username) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: "Username is required when Authorization Type is BASIC AUTH",
+          path: ["basic_auth_username"],
+        });
+      }
+      if (!val.basic_auth_password) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: "Password is required when Authorization Type is BASIC AUTH",
+          path: ["basic_auth_password"],
+        });
+      }
+      break;
+    }
+  }
+}
+
+export function validatePaginationType(
+  val: CommonApiIngestionFormSchema,
+  ctx: z.RefinementCtx,
+) {
+  switch (val.pagination_type) {
+    case PaginationTypeEnum.LIMIT_OFFSET: {
+      if (!val.size || val.size < 1) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: "Size is required when Pagination Method is LIMIT OFFSET",
+          path: ["size"],
+        });
+      }
+      if (!val.page_size_key) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message:
+            "Page Size Key is required when Pagination Method is LIMIT OFFSET",
+          path: ["page_size_key"],
+        });
+      }
+      if (!val.page_offset_key) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message:
+            "Page Offset Key is required when Pagination Method is LIMIT OFFSET",
+          path: ["page_offset_key"],
+        });
+      }
+      break;
+    }
+    case PaginationTypeEnum.PAGE_NUMBER: {
+      if (!val.size || val.size < 1) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: "Size is required when Pagination Method is PAGE NUMBER",
+          path: ["size"],
+        });
+      }
+      if (!val.page_size_key) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message:
+            "Page Size Key is required when Pagination Method is PAGE NUMBER",
+          path: ["page_size_key"],
+        });
+      }
+      if (!val.page_number_key) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message:
+            "Page Number Key is required when Pagination Method is PAGE NUMBER",
+          path: ["page_number_key"],
+        });
+      }
+      if (!val.page_starts_with || val.page_starts_with < 0) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message:
+            "Page Starts With must be greater than or equal to 0 when Pagination Method is PAGE NUMBER",
+          path: ["page_starts_with"],
+        });
+      }
+      break;
+    }
+  }
+}
