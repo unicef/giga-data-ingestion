0329ab8d9ff20164e5a52f9084fc8b79ab745382
Kenneth Domingo <kenneth@thinkingmachin.es>
2024-05-20 13:42:08 +0800

    fix(api,ui): UAT fixes (#99)
    * fix(ui): disable refetch on window focus
    
    * fix(ui): disable auto retry
    
    * fix(api/ui): dq checks sorting, truncate decimal percent

--- ui/src/api/index.ts
+++ ui/src/api/index.ts
@@ -135,9 +135,13 @@ export function AxiosProvider({ children }: PropsWithChildren) {
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      refetchOnWindowFocus: true,
-      refetchOnReconnect: true,
+      retry: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
       placeholderData: keepPreviousData,
+    },
+    mutations: {
+      retry: false,
     },
   },
 });
