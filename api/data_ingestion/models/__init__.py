9de47611bc3ec7e6c4acf4fb7f659fd31687fd24
gerlito chagas <ger@thinkingmachin.es>
2024-08-02 22:05:27 +0800

    feat: export UserRoleAssociation table

--- api/data_ingestion/models/__init__.py
+++ api/data_ingestion/models/__init__.py
@@ -3,7 +3,7 @@ from .base import BaseModel
 from .deletion_requests import DeletionRequest
 from .file_upload import FileUpload
 from .ingest_api_qos import ApiConfiguration, SchoolConnectivity, SchoolList
-from .users import Role, User
+from .users import Role, User, UserRoleAssociation
 
 __all__ = [
     "BaseModel",
@@ -15,4 +15,5 @@ __all__ = [
     "DeletionRequest",
     "User",
     "Role",
+    "UserRoleAssociation",
 ]
