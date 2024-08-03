ea843c583cfb704b1bbb95178252f09e2fadef75
Gerlito Chagas <ger@thinkingmachin.es>
2024-08-04 00:37:36 +0800

    fix: handle null data and memoize user enabled value

--- ui/src/hooks/useUser.ts
+++ ui/src/hooks/useUser.ts
@@ -1,15 +1,20 @@
+import { useMemo } from "react";
+
 import { useQuery } from "@tanstack/react-query";
 
 import { api } from "@/api";
 
 function useUser() {
-  const { data: rolesQuery, isFetching } = useQuery({
+  const { data: userQuery, isFetching } = useQuery({
     queryKey: ["user", "me"],
     queryFn: api.users.getCurrentUser,
     staleTime: 60 * 5 * 1000,
   });
 
-  const enabled = rolesQuery?.data.enabled;
+  const enabled = useMemo(
+    () => userQuery?.data?.enabled ?? false,
+    [userQuery?.data],
+  );
 
   return {
     enabled,
