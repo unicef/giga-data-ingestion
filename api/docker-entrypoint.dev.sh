c6f46b0408b1b263f6616cc15234d398076f2921
Kenneth V. Domingo <kenneth@thinkingmachin.es>
2024-08-05 15:47:15 +0800

    build(api): load roles fixtures on dev startup

--- api/docker-entrypoint.dev.sh
+++ api/docker-entrypoint.dev.sh
@@ -4,6 +4,6 @@ set -eu
 
 poetry install --no-root --no-interaction --no-ansi --with dev
 poetry run alembic upgrade head
-poetry run python -m scripts.load_fixtures approval_requests
+poetry run python -m scripts.load_fixtures roles approval_requests
 
 exec poetry run uvicorn main:app --host 0.0.0.0 --port 8000 --workers 2 --reload
