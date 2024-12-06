fe02b087484794b018354dc73bec2aaa7501d5d8
Brenda  Mukindia <mukindiabrenda67@gmail.com>
2024-12-06 03:31:15 +0300

    adding upload types for unstructured data

--- ui/src/constants/upload.ts
+++ ui/src/constants/upload.ts
@@ -3,6 +3,9 @@ export enum AcceptedFileTypes {
   JSON = ".json",
   EXCEL_LEGACY = ".xls",
   EXCEL = ".xlsx",
+  PDF = ".pdf",
+  DOC = ".doc",
+  DOCX = ".docx",
 }
 
 export enum AcceptedUnstructuredFileTypes {
@@ -13,6 +16,12 @@ export enum AcceptedUnstructuredFileTypes {
   PNG = ".png",
   TIF = ".tif",
   TIFF = ".tiff",
+  CSV = ".csv",
+  EXCEL_LEGACY = ".xls",
+  EXCEL = ".xlsx",
+  PDF = ".pdf",
+  DOC = ".doc",
+  DOCX = ".docx",
 }
 
 export const AcceptedUnstructuredMimeTypes = [
@@ -21,12 +30,18 @@ export const AcceptedUnstructuredMimeTypes = [
   "image/jpeg",
   "image/png",
   "image/tiff",
+  "application/vnd.ms-excel",
+  "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+  "application/pdf",
+  "application/msword",
+  "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
 ];
 
 export const UPLOAD_MODE_OPTIONS = ["Create", "Update"] as const;
-export type UploadModeOptions = typeof UPLOAD_MODE_OPTIONS[number];
+export type UploadModeOptions = (typeof UPLOAD_MODE_OPTIONS)[number];
 
 export const MAX_UPLOAD_FILE_SIZE_MB = 10;
 
 export const MAX_UPLOAD_FILE_SIZE_BYTES =
   MAX_UPLOAD_FILE_SIZE_MB * (2 ** 10) ** 2;
+
