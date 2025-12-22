8f88f35e382d7c26c28ec184fe6153094f1f690f
Bidhan Mondal <bidhan.mondal@nagarro.com>
2025-12-22 10:59:13 +0530

    chore: Changed the scripts path and added to infra init jobs yaml

--- api/docker-entrypoint.dev.sh
+++ api/docker-entrypoint.dev.sh
@@ -5,6 +5,5 @@ set -eu
 poetry install --no-root --no-interaction --no-ansi --with dev
 poetry run alembic upgrade head
 poetry run python -m scripts.load_fixtures roles approval_requests
-poetry run python -m scripts.migrate_blob_metadata --apply
 
 exec poetry run uvicorn main:app --host 0.0.0.0 --port 8000 --workers 2 --reload
