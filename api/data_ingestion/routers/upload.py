94c468def091871091e7eec78cd5fa23e6292933
bidhan-nagarro <bidhan.mondal@nagarro.com>
2026-02-10 23:14:46 +0530

    feat: create separate file for upload metadata
    * :sparkles: feat: Added migration for old data without metadata json file.
    
    * :wrench: chore: Update to refer to metadata.json from the blob
    
    * :fire: chore: Update sh file to run migrate code for older countries
    
    * :fire: chore: Migration file updated
    
    * :fire: chore: Migration file updated
    
    * :fire: chore: Migration file updated
    
    * :fire: chore: added command for migrating old metadata
    
    * chore: Addressed Review Comments
    
    * chore: Addressed Review Comments
    
    * chore: Addressed Review Comments
    
    * chore: Addressed Review Comments
    
    * chore: Changed the scripts path and added to infra init jobs yaml
    
    * chore: Fixed metadata path issue.
    
    * chore: Removed code for error fix in ApprovelRequest Model
    
    * chore: Removed code for error fix in FileUpload Model
    
    * chore: Removed code for error fix in ApprovalRequest Model
    
    * refactor: Change in the init jobs to add migrate as a separate job
    
    * chore: disable migrate blob job
    
    * chore: re-enable migrate blob job
    
    * refactor: migrate script modified
    
    * refactor: migrate script modified
    
    * refactor: migrate script modified
    
    * refactor: Migrate Script fixed
    
    * refactor: Migrate Script fixed
    
    * chore: make pre-commit fixes
    
    ---------
    
    Co-authored-by: Brian Musisi <brianmusisi@berkeley.edu>

--- api/data_ingestion/routers/upload.py
+++ api/data_ingestion/routers/upload.py
@@ -8,6 +8,8 @@ import country_converter as coco
 import magic
 import orjson
 import pandas as pd
+from azure.core.exceptions import HttpResponseError
+from azure.storage.blob import ContentSettings
 from data_ingestion.constants import constants
 from data_ingestion.db.primary import get_db
 from data_ingestion.internal.auth import azure_scheme
@@ -34,6 +36,7 @@ from data_ingestion.schemas.upload import (
     UploadSummaryResponse,
 )
 from data_ingestion.utils.data_quality import get_metadata_path
+from data_ingestion.utils.data_quality import get_metadata_path
 from fastapi import (
     APIRouter,
     Depends,
@@ -49,10 +52,6 @@ from pydantic import Field
 from sqlalchemy import delete, func, select
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.responses import StreamingResponse
-
-from azure.core.exceptions import HttpResponseError
-from azure.storage.blob import ContentSettings
-from data_ingestion.utils.data_quality import get_metadata_path
 
 router = APIRouter(
     prefix="/api/upload",
