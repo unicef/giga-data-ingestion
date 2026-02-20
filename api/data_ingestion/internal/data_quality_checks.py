6c86831170a4e5411a5577bdb648787a3922d443
Brian Musisi <brianmusisi@berkeley.edu>
2026-02-20 17:05:56 +0100

    refactor: update to read dq as parquet (#202)
    * refactor: update to read dq as parquet
    
    * fix: add pandas parquet support

--- api/data_ingestion/internal/data_quality_checks.py
+++ api/data_ingestion/internal/data_quality_checks.py
@@ -1,5 +1,5 @@
 import json
-from io import StringIO
+from io import BytesIO
 
 import pandas as pd
 from fastapi import (
@@ -67,9 +67,8 @@ def get_first_n_error_rows_for_data_quality_check(
         metadata = dict(props.metadata or {})
 
     blob_data = blob.download_blob().readall()
-    data_str = blob_data.decode("utf-8")
-    data_io = StringIO(data_str)
-    df = pd.read_csv(data_io)
+    data_io = BytesIO(blob_data)
+    df = pd.read_parquet(data_io)
 
     for column in df.columns:
         column_result = process_n_columns(column, df, rows_to_process)
