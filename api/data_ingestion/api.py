d3cc13d1c744c747fa836454bd73bc07a46ebf81
Javiershenbc <jashen@unicef.org>
2026-03-19 17:25:51 +0100

    fix: fix lint errors on import orders from ruff
    fix lint errors on import orders from ruff

--- api/data_ingestion/api.py
+++ api/data_ingestion/api.py
@@ -2,6 +2,11 @@ import logging
 import sys
 from datetime import timedelta
 
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse, ORJSONResponse
+from starlette.middleware.sessions import SessionMiddleware
+
 from data_ingestion.constants import __version__
 from data_ingestion.db.primary import get_db_context
 from data_ingestion.internal.auth import azure_scheme, local_auth_bypass
@@ -21,10 +26,6 @@ from data_ingestion.routers import (
     utils,
 )
 from data_ingestion.settings import DeploymentEnvironment, initialize_sentry, settings
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import FileResponse, ORJSONResponse
-from starlette.middleware.sessions import SessionMiddleware
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
--- api/data_ingestion/routers/error_table.py
+++ api/data_ingestion/routers/error_table.py
@@ -1,7 +1,5 @@
 import io
 
-from data_ingestion.db.trino import get_db
-from data_ingestion.internal.auth import azure_scheme
 from fastapi import (
     APIRouter,
     Depends,
@@ -13,6 +11,9 @@ from fastapi import (
 from sqlalchemy import column, func, literal, select, text
 from sqlalchemy.orm import Session
 from starlette.responses import StreamingResponse
+
+from data_ingestion.db.trino import get_db
+from data_ingestion.internal.auth import azure_scheme
 
 router = APIRouter(
     prefix="/api/error-table",
