ae3d484c4158f0e59662e65eff80285bb59cb4c4
Brian Musisi <brianmusisi@berkeley.edu>
2026-04-01 13:57:43 +0300

    remove unused get n errors check

--- ui/src/components/check-file-uploads/DataCheckItem.tsx
+++ ui/src/components/check-file-uploads/DataCheckItem.tsx
@@ -2,19 +2,18 @@ import { useState } from "react";
 
 import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@carbon/react";
 
-import { Check, DqFailedRowsFirstFiveRows } from "@/types/upload";
+import { Check } from "@/types/upload";
 
 import DataQualityChecks from "./ColumnChecks";
 
 interface DataCheckItemProps {
   data: Check[];
-  previewData: DqFailedRowsFirstFiveRows;
   title?: string;
   uploadId: string;
   hasDownloadButton?: boolean;
 }
 
-const DataCheckItem = ({ data, previewData, title }: DataCheckItemProps) => {
+const DataCheckItem = ({ data, title }: DataCheckItemProps) => {
   const [selectedTab, setSelectedTab] = useState(0);
   return (
     <Tabs
@@ -29,7 +28,7 @@ const DataCheckItem = ({ data, previewData, title }: DataCheckItemProps) => {
 
       <TabPanels>
         <TabPanel>
-          <DataQualityChecks data={data} previewData={previewData} />
+          <DataQualityChecks data={data} />
         </TabPanel>
       </TabPanels>
     </Tabs>
