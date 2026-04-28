37dda9a4501b37a7a6b72c530a639619809e58c3
Gaurav Gupta <GGupta5@schools.nyc.gov>
2026-04-28 21:19:45 +0530

    feat: duplicate validate_fuzzy issue fixed

--- ui/src/api/routers/uploads.ts
+++ ui/src/api/routers/uploads.ts
@@ -70,21 +70,6 @@ export default function routes(axi: AxiosInstance) {
       });
     },
 
-    validate_fuzzy: (
-      params: FuzzyValidationParams,
-    ): Promise<AxiosResponse<FuzzyValidationResponse>> => {
-      const formData = new FormData();
-      formData.append("file", params.file);
-      formData.append(
-        "column_to_schema_mapping",
-        params.column_to_schema_mapping,
-      );
-
-      return axi.post("/upload/validate-fuzzy", formData, {
-        params: { dataset: params.dataset },
-      });
-    },
-
     upload_unstructured: (
       params: UploadUnstructuredParams,
     ): Promise<AxiosResponse<null>> => {
